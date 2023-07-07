32f961cc505b7aa5f1dc9dec259227ee0ef1668a
koushith <koushith97@gmail.com>
2023-07-07 19:44:15 +0530

    changed .env

--- frontend/src/pages/suggestions/suggestions.page.tsx
+++ frontend/src/pages/suggestions/suggestions.page.tsx
@@ -5,6 +5,7 @@ import { Configuration, OpenAIApi } from "openai";
 import { useEffect, useState } from "react";
 
 import {
+  BACKEND_BASE_URL,
   OPEN_AI_API_KEY,
   YOUTUBE_API_KEY,
   extractYouTubeVideoId,
@@ -29,7 +30,7 @@ export const SuggestionsPage = () => {
   const { userInfo } = useAuth();
   console.log(userInfo);
 
-  console.log("history coming from context", history);
+  console.log("backend base url", BACKEND_BASE_URL);
 
   const addSearchResult = async (
     videoTitle: string,
@@ -38,15 +39,12 @@ export const SuggestionsPage = () => {
     email: string
   ) => {
     try {
-      const record = await axios.post(
-        `${process.env.REACT_APP_BACKEND_BASE}/video`,
-        {
-          videoTitle,
-          videoLink,
-          name,
-          email,
-        }
-      );
+      const record = await axios.post(`${BACKEND_BASE_URL}/video`, {
+        videoTitle,
+        videoLink,
+        name,
+        email,
+      });
       console.log("reord inserted?", record);
     } catch (error) {
       console.log("something went wrong", error);
@@ -57,9 +55,7 @@ export const SuggestionsPage = () => {
     try {
       let userID = userInfo?.email;
 
-      const query = await axios.get(
-        `${process.env.REACT_APP_BACKEND_BASE}/video/${userID}`
-      );
+      const query = await axios.get(`${BACKEND_BASE_URL}/video/${userID}`);
       console.log("queryyyyyyyyyyyyyyyy-------", query.data.videoResults);
 
       console.log("query", query.data.videoResults);
