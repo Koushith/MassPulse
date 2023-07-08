a7a3bee948a27812189ff1ac7ce22c259255af28
koushith <koushith97@gmail.com>
2023-07-08 15:36:53 +0530

    adds cors

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,21 @@ import express from "express";
 import connectDB from "./utils/db.js";
 import cors from "cors";
 import {
-  addNewVideo,
-  getAllVideos,
-  getPreviousResponseById,
-  updateResponse,
+    addNewVideo,
+    getAllVideos,
+    getPreviousResponseById,
+    updateResponse,
 } from "./controllers/video/video.controller.js";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 const corsConfig = {
-  origin: [
-    "https://masspulse.xyz",
-    "http://localhost:3000",
-    "http://localhost:8000",
-  ],
+    origin: [
+        "https://masspulse.xyz",
+        "http://localhost:3000",
+        "http://localhost:8000",
+    ],
 };
 
 app.use(cors(corsConfig));
@@ -28,9 +28,9 @@ app.post("/video", addNewVideo);
 app.post("/video/update", updateResponse);
 app.get("/video/search/:videoId", getPreviousResponseById);
 app.get("/", (_, res) => {
-  res.send("This Route works!!");
+    res.send("This Route works!!");
 });
 
 app.listen(PORT, () => {
-  console.log(`App is runing on the Port---${PORT}`);
+    console.log(`App is runing on the Port--${PORT}`);
 });
