8b55a9996bf2f71e8df8b263af7d38291e1dc09e
koushith <koushith97@gmail.com>
2023-07-11 15:16:35 +0530

    adds token limiter

--- frontend/src/utils/token-limiter.ts
+++ frontend/src/utils/token-limiter.ts
@@ -1,1 +1,9 @@
-export {};
+function calculateTokenCount(wordCount: any) {
+  const averageCharsPerWord = 5; // Assuming an average of 5 characters per word
+  const charsPerToken = 4; // Each token consists of 4 characters
+
+  const estimatedTokenCount = Math.ceil(
+    (wordCount * averageCharsPerWord) / charsPerToken
+  );
+  return estimatedTokenCount;
+}
